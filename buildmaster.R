5edd4b556c9777c498d13c43ae3cd3351d00303e
robinlovelace <rob00x@gmail.com>
2016-02-08 10:57:15 +0000

    Minor updates to build scripts for custom regions

--- buildmaster.R
+++ buildmaster.R
@@ -1,20 +1,26 @@
 source("set-up.R")
 library(knitr)
 
+# For PCT regions:
 regions <- readOGR("../pct-bigdata/regions.geojson", layer = "OGRGeoJSON")
 la_all <- regions$Region <- as.character(regions$Region)
-
 # select regions of interest (uncomment/change as appropriate)
 # sel <- c("cambridge", "hereford", "northumberland", "devon")
 # la_all <- regions$Region[charmatch(sel, regions$Region)]
 # la_all <- as.character(la_all)
 # la_all <- c("liverpool-city-region")
 
+# # For custom regions:
+# regions <- shapefile("/tmp/Study_Areas.shp")
+# regions$Region <- tolower(regions$Name) # add region names
+# la_all <- regions$Region
+
 for(k in 1:length(la_all)){
   # What geographic level are we working at (cua or regional)
-  geo_level <- "cua"
-  isolated <- FALSE
+  geo_level <- "custom"
+  isolated <- TRUE
   region <- la_all[k]
+  message(paste0("Building for ", region))
   knitr::knit2html(quiet = T,
     input = "load.Rmd",
     output = file.path("../pct-data/", region, "/model-output.html"),
@@ -30,9 +36,10 @@ for(k in 1:length(la_all)){
 la_all <- la_all[-grep("liv|greater-m", la_all)]
 
 # old regional units
-# las <- readOGR(dsn = "pct-bigdata/national/cuas-mf.geojson", layer = "OGRGeoJSON")
-# las_names <- las$CTYUA12NM
-# las_names <- las_names[order(las_names)]
-# las_names <- as.character(las_names)
-# head(las_names)
+las <- readOGR(dsn = "../pct-bigdata/cuas-mf.geojson", layer = "OGRGeoJSON")
+las_names <- las$CTYUA12NM
+las$Region <- tolower(as.character(las_names))
+la_all <- region <- "leeds"
+regions <- las[las$Region == region,]
+plot(regions)
 # dput(las_names[1:4])
