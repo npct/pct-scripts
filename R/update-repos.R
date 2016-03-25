c75bc9a92ceec8d4e4f7a2ca21b37a9db556132e
robinlovelace <rob00x@gmail.com>
2016-03-25 22:41:57 +0000

    Delete update repos script

--- R/update-repos.R
+++
@@ -1,9 +0,0 @@
-# Aim: pull latest versions of the repositories needed to build the PCT
-
-# Testing git2r for using the repo
-library(git2r)
-repo = repository(".")
-workdir(repo)
-commits(repo)[[1]]
-
-add()
