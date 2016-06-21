9cfe05dd58df05df6af5e475d9ab0e0f2e7c68ac
robinlovelace <rob00x@gmail.com>
2016-06-21 09:35:00 +0100

    Update download-data script (was out of date)

--- download-data.R
+++ download-data.R
@@ -1,15 +1,17 @@
-# Get external data for pct - not downloaded from GH
+# Get external data for pct - not downloaded via git clone
 fname = "../pct-bigdata/rf.Rds"
+file.size(fname) / 1000000
+file.mtime(fname)
 if(!file.exists(fname))
-  download.file("https://github.com/npct/pct-bigdata/releases/download/0.1/rf.Rds",
+  curl::curl_download("https://github.com/npct/pct-bigdata/releases/download/0.2/rf.Rds",
               fname, mode = "wb")
 fname = "../pct-bigdata/rq.Rds"
 if(!file.exists(fname))
-  download.file("https://github.com/npct/pct-bigdata/releases/download/0.1/rq.Rds", 
+  curl::curl_download("https://github.com/npct/pct-bigdata/releases/download/0.2/rq.Rds", 
               fname, mode = "wb")
 fname = "../pct-bigdata/l_all_cc.Rds"
 if(!file.exists(fname))
-  download.file("https://github.com/npct/pct-bigdata/releases/download/0.1/l_all_cc.Rds",
+  curl::curl_download("https://github.com/npct/pct-bigdata/releases/download/0.1/l_all_cc.Rds",
               fname, mode = "wb")
 
 fname = "../pct-bigdata/pct_lines.csv"
