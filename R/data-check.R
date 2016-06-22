59f85f5975279ce2fc120ea1dca2a7171c3ba31c
robinlovelace <rob00x@gmail.com>
2016-06-22 11:44:04 +0100

    Update data check script

--- R/data-check.R
+++ R/data-check.R
@@ -1,5 +1,5 @@
 # Aim: check the data produced by pct-load
-
+source("set-up.R")
 # check national lines data
 # after downloading latest data from github, e.g. with download-data.R
 fname = "../pct-bigdata/rf.Rds"
@@ -7,12 +7,32 @@ fname = "../pct-bigdata/rq.Rds"
 fname = "../pct-bigdata/l_all_cc.Rds"
 r = readRDS(fname)
 
-# Are the lengths crazy long?
-summary(r$length > 50)
-plot(r[r$length > 50,])
+# Which lengths are crazy long?
+sel = r$length > 100 | is.na(r$length)
+summary(sel)
+plot(r[sel,])
+summary(r$length[sel])
 
-r = readRDS(fname)
-summary(r$dist_quiet)
+# Fix the lengths (out be 3 orders of magnitude)
+r$length[sel] = r$length[sel] / 1000
 
+# save the result
+# saveRDS(r, fname)
 
-
+# checks per region
+pct_data <- file.path("..", "pct-data")
+regions <- readOGR("../pct-bigdata/regions.geojson", layer = "OGRGeoJSON")
+la_all <- regions$Region <- as.character(regions$Region)
+i = 1
+offending_region = NULL
+for(i in 1:length(la_all)){
+  region_shape = regions[i,]
+  region_name = region_shape$Region
+  
+  zones = readRDS(paste0("../pct-data/", region_name, "/z.Rds"))
+  rq = readRDS(paste0("../pct-data/", region_name, "/rq.Rds"))
+  if(max(rq$length, na.rm = T) > 100){
+    print(region_name)
+    offending_region = c(offending_region, region_name)
+  }
+}
