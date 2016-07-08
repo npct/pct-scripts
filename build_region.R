484917e3afff03a2c92f4224832bacb3baef307c
Nikolai B <nikolai-b@users.noreply.github.com>
2016-07-08 14:22:44 +0100

    Remove unneeded columns. Use lowercase consistently
    Ref npct/pct#23 and ref npct/pct-bigdata#1 and ref npct/pct-shiny#276

--- build_region.R
+++ build_region.R
@@ -3,6 +3,9 @@ source("set-up.R") # load packages needed
 # ms_simplify gives Error: RangeError: Maximum call stack size exceeded
 # for large objects.  Turning the repair off fixed it...
 too_large <- function(to_save, max_size = 5.6){ format(object.size(to_save), units = 'Mb') > max_size }
+remove_cols <- function(df, col_regex){
+  df[,!grepl(col_regex, names(df))]
+}
 
 # Create default LA name if none exists
 start_time <- Sys.time() # for timing the script
@@ -50,28 +53,32 @@ zones <- ukmsoas[ukmsoas@data$geo_code %in% cents$geo_code, ]
 # load flow dataset, depending on availability
 if(!exists("flow_nat"))
   flow_nat <- readRDS(file.path(pct_bigdata, "pct_lines_oneway_shapes.Rds"))
-summary(flow_nat$dutch_slc / flow_nat$All)
+summary(flow_nat$dutch_slc / flow_nat$all)
 
-if(!exists("rf_nat"))
+if(!exists("rf_nat")){
   rf_nat <- readRDS(file.path(pct_bigdata, "rf.Rds"))
-if(!exists("rq_nat"))
+  rf_nat <- remove_cols(rf_nat, "(waypoint|co2_saving|calories|busyness|plan|start|finish)")
+}
+if(!exists("rq_nat")){
   rq_nat <- readRDS(file.path(pct_bigdata, "rq.Rds"))
+  rq_nat <- remove_cols(rq_nat, "(waypoint|co2_saving|calories|busyness|plan|start|finish)")
+}
 # Subset by zones in the study area
-o <- flow_nat$Area.of.residence %in% cents$geo_code
-d <- flow_nat$Area.of.workplace %in% cents$geo_code
+o <- flow_nat$msoa1 %in% cents$geo_code
+d <- flow_nat$msoa2 %in% cents$geo_code
 flow <- flow_nat[o & d, ] # subset OD pairs with o and d in study area
 
 # Remove Webtag, increase in walkers and base_
-zones <- zones[,!grepl("(webtag|siw$|base_)", names(zones@data))]
-flow <- flow[,!grepl("(webtag|siw$|base_)", names(zones@data))]
+zones <- remove_cols(zones, "(webtag|siw$|sld$|siw$|base_)")
+flow <- remove_cols(flow, "(webtag|siw$|sld$|siw$|base_)")
 
 params$n_flow_region <- nrow(flow)
-params$n_commutes_region <- sum(flow$All)
+params$n_commutes_region <- sum(flow$all)
 
 # Subset lines
 # subset OD pairs by n. people using it
-params$sel_long <- flow$All > params$mflow & flow$dist < params$mdist
-params$sel_short <- flow$dist < params$max_all_dist & flow$All > params$mflow_short
+params$sel_long <- flow$all > params$mflow & flow$dist < params$mdist
+params$sel_short <- flow$dist < params$max_all_dist & flow$all > params$mflow_short
 sel <- params$sel_long | params$sel_short
 flow <- flow[sel, ]
 # summary(flow$dist)
@@ -79,13 +86,13 @@ flow <- flow[sel, ]
 l <- flow
 
 # add geo_label of the lines
-l$geo_label_o = left_join(l@data["Area.of.residence"], zones@data[c("geo_code", "geo_label")], by = c("Area.of.residence" = "geo_code"))[[2]]
-l$geo_label_d = left_join(l@data["Area.of.workplace"], zones@data[c("geo_code", "geo_label")], by = c("Area.of.workplace" = "geo_code"))[[2]]
+l$geo_label_o = left_join(l@data["msoa1"], zones@data[c("geo_code", "geo_label")], by = c("msoa1" = "geo_code"))[[2]]
+l$geo_label_d = left_join(l@data["msoa2"], zones@data[c("geo_code", "geo_label")], by = c("msoa2" = "geo_code"))[[2]]
 
 # proportion of OD pairs in min-flow based subset
 params$pmflow <- round(nrow(l) / params$n_flow_region * 100, 1)
 # % all trips covered
-params$pmflowa <- round(sum(l$All) / params$n_commutes_region * 100, 1)
+params$pmflowa <- round(sum(l$all) / params$n_commutes_region * 100, 1)
 
 rf_nat$id <- gsub('(?<=[0-9])E', ' E', rf_nat$id, perl=TRUE) # temp fix to ids
 rq_nat$id <- gsub('(?<=[0-9])E', ' E', rq_nat$id, perl=TRUE)
@@ -99,14 +106,8 @@ l$time_fast <- rf$time
 l$time_quiet <- rq$time
 l$cirquity <- rf$length / l$dist
 l$distq_f <- rq$length / rf$length
-l$avslope <- rf$av_incline
-l$co2_saving <- rf$co2_saving
-l$calories <- rf$calories
-l$busyness <- rf$busyness
-l$avslope_q <- rq$av_incline
-l$co2_saving_q <- rq$co2_saving
-l$calories_q <- rq$calories
-l$busyness_q <- rq$busyness
+l$avslope <- rf$av_incline * 100
+l$avslope_q <- rq$av_incline * 100
 
 rft <- rf
 # Stop rnet lines going to centroid (optional)
@@ -115,7 +116,7 @@ if(length(rft) == length(rf)){
   row.names(rft) <- row.names(rf)
   rft <- SpatialLinesDataFrame(rft, rf@data)
 } else print("Error: toptailed lines do not match lines")
-rft$Bicycle <- l$Bicycle
+rft$bicycle <- l$bicycle
 
 # Simplify line geometries (if mapshaper is available)
 # this greatly speeds up the build (due to calls to overline)
@@ -127,12 +128,12 @@ if (rft_too_large){
   file.create(file.path(pct_data, region, "rft_too_large"))
 }
 
-rnet <- overline(rft, "Bicycle")
+rnet <- overline(rft, "bicycle")
 
 if(require(foreach) & require(doParallel)){
   n_cores <- 4 # set max number of cores to 4
   # reduce n_cores for 2 core machines
-  if(parallel:::detectCores() < 4) 
+  if(parallel:::detectCores() < 4)
     n_cores <- parallel:::detectCores()
   cl <- makeCluster(n_cores)
   registerDoParallel(cl)
@@ -165,7 +166,7 @@ rnet_cents <- SpatialLinesMidPoints(rnet_osgb)
 rnet_cents <- spTransform(rnet_cents, CRS("+init=epsg:4326"))
 
 proj4string(rnet) = proj4string(zones)
-for(i in c("Bicycle", scens)){
+for(i in c("bicycle", scens)){
   nm = paste0(i, "_upto") # new variable name
   zones@data[nm] = left_join(zones@data[c("geo_code")], cents@data[c("geo_code", i)])[2]
   rnet@data = cbind(rnet@data, over(rnet_cents, zones[nm]))
@@ -185,8 +186,7 @@ if(!"gendereq_slc" %in% scens)
 # # # # # # # # #
 
 # Remove/change private/superfluous variables
-l$Male <- l$Female <- l$From_home <- l$calories <-
-  l$co2_saving_q <-l$calories_q <- l$busyness_q <-
+l$Male <- l$Female <- l$From_home <-
   # data used in the model - superflous for pct-shiny
   l$dist_fastsq <- l$dist_fastsqrt <- l$ned_avslope <-
   l$interact <- l$interactsq <- l$interactsqrt <- NULL
@@ -195,7 +195,7 @@ l$Male <- l$Female <- l$From_home <- l$calories <-
 l$avslope <- l$avslope * 100
 
 # Creation of clc current cycling variable (temp)
-l$clc <- l$Bicycle / l$All * 100
+l$clc <- l$bicycle / l$all * 100
 
 # Transfer cents data to zones
 cents@data$avslope <- NULL
@@ -210,7 +210,7 @@ for(ii in na_cols){
 
 # # Save objects
 # Save objects # uncomment these lines to save model output
-save_formats <- function(to_save, name = F){
+save_formats <- function(to_save, name = F, csv = F){
   if (name == F){
     name <- substitute(to_save)
   }
@@ -222,7 +222,7 @@ save_formats <- function(to_save, name = F){
 
   # Simplify geom
   geojson_write( ms_simplify(to_save, keep = 0.1, no_repair = too_large(to_save)), file = file.path(pct_data, region, name))
-  write.csv(to_save@data, file.path(pct_data, region, paste0(name, ".csv")))
+  if(csv) write.csv(to_save@data, file.path(pct_data, region, paste0(name, ".csv")))
 }
 
 round_df <- function(df, digits) {
@@ -233,9 +233,9 @@ round_df <- function(df, digits) {
   (df)
 }
 
-save_formats(zones, 'z')
+save_formats(zones, 'z', csv = T)
 rm(zones)
-save_formats(l)
+save_formats(l, csv = T)
 rm(l)
 save_formats(rf)
 rm(rf)
