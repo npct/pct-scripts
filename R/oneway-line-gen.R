ced876712c80fefe6da3048e887918311c6627f5
robinlovelace <rob00x@gmail.com>
2016-07-18 15:50:52 +0100

    Update tests for Cambridge

--- R/oneway-line-gen.R
+++ R/oneway-line-gen.R
@@ -1,15 +1,16 @@
 # Aim: find and re-add 'missing lines'
 
 # Start: get data for Cambridge
+source("set-up.R")
 region = "cambridgeshire"
-source("build_region.R") # comment out to skip build
+# source("build_region.R") # comment out to skip build
 
 # load excel file containing them
 download.file("https://github.com/npct/pct-shiny/files/333471/160624_MissingCambLines.xlsx", "160624_MissingCambLines.xlsx")
 flow_ag = readxl::read_excel("160624_MissingCambLines.xlsx")
 l = readRDS("../pct-data/cambridgeshire/l.Rds")
 zones = readRDS("../pct-data/cambridgeshire/z.Rds")
-
+cents = readRDS("../pct-data/cambridgeshire/c.Rds")
 
 sum(flow_ag$visualised_anna)
 nrow(l)
@@ -24,39 +25,44 @@ o <- flow_cens$`Area of residence` %in% cents$geo_code
 d <- flow_cens$`Area of workplace` %in% cents$geo_code
 flow_cam <- flow_cens[o & d, ] # subset OD pairs with o and d in study area
 # Remove flows with all < 10
-flow_cam = flow_cam[flow_cam$`All categories: Method of travel to work` > 10,]
+# flow_cam = flow_cam[flow_cam$`All categories: Method of travel to work` > 10,]
 # Convert to SpatialLines
 flow_cam_sp = od2line(flow = flow_cam, zones = zones)
 plot(flow_cam_sp)
 flow_cam_sp_osgb = spTransform(flow_cam_sp, CRSobj = CRS("+init=epsg:27700"))
 flow_cam_sp$dist = gLength(flow_cam_sp_osgb, byid = T) / 1000
 summary(flow_cam_sp$dist)
-flow_cam_sp = flow_cam_sp[flow_cam_sp$dist < 20,]
-# Finding: 2304 lines
+flow_cam_sp = flow_cam_sp[flow_cam_sp$dist < 20 & flow_cam_sp$dist > 0.5,]
+plot(flow_cam_sp, lwd = flow_cam_sp$`All categories: Method of travel to work` /
+       mean(flow_cam_sp$`All categories: Method of travel to work`))# Finding: 2304 lines
 sum(flow_cam_sp$`All categories: Method of travel to work`)
 # Finding: 227166 commuters in Cambridgeshire
 sum(l$all)
 # Finding: 175204 commuters, 77% of Census data
 
 
-flow_cam_oneway = onewayid(flow_cam_sp, attrib = 3:ncol(flow_cam_sp))
-# Finding: results in 1294 flows: lines are lost in onewayid
 # flow_cam_oneway = onewaygeo(flow_cam_sp, attrib = 3:ncol(flow_cam_sp)) # finding: fewer flows still
-sum(flow_cam_oneway$`All categories: Method of travel to work`)
+# Finding: results in 1294 flows: lines are lost in original onewayid
 
-# Solution: create new onewayid method, by creating a new id column
-# Test case (to be removed)
-ftest = data.frame(o = c(1, 2, 2), d = c(2, 1, 3), all = 10:8) # 3 lines
-ftest$id1 = paste(ftest$o, ftest$d)
-ftest$id2 = paste(ftest$d, ftest$o)
-ftest$two_way = ftest$id2 %in% ftest$id1 # identify the 2 way flows
-# save the 1 way flows
-ftest_oneway = ftest[!ftest$two_way,]
-ftest_twoway = ftest[ftest$two_way,]
-u = unique(ftest$id1)
-for(i in 1:length(u)){
-  if(sum(ftest$id1 == u[i]) == 1)
-    ftest$id1[ftest$id1 == u[i]] = ftest$id2[ftest$id1 == u[i]]
-}
-test_oneway = group_by(ftest, id1) %>% 
-  summarise(all = sum(all))
+# Solution: update stplanr::onewayid() function
+flow_cam_oneway = onewayid(flow_cam_sp@data, attrib = 3:14)
+# flow_cam_oneway = onewayid(flow_cam_sp@data, attrib = 3:ncol(flow_cam_sp))
+
+sum(flow_cam_oneway$`All categories: Method of travel to work`) ==
+  sum(flow_cam_sp$`All categories: Method of travel to work`)
+# Finding: fixed, they have the same total flow now
+flow_cam_oneway = flow_cam_oneway[flow_cam_oneway$`All categories: Method of travel to work` > 10,]
+summary(flow_cam_oneway$dist)
+flow_cam_oneway$dist = flow_cam_oneway$dist / 2
+
+flow_cam_oneway_sp = od2line(flow_cam_oneway, cents)
+flow_cam_sp_osgb = spTransform(flow_cam_oneway_sp, CRSobj = CRS("+init=epsg:27700"))
+flow_cam_oneway_sp$dist2 = gLength(flow_cam_sp_osgb, byid = T) / 1000
+plot(flow_cam_oneway_sp$dist2, flow_cam_oneway_sp$dist)
+
+summary(flow_cam_oneway_sp$dist2)
+plot(flow_cam_oneway_sp, lwd = flow_cam_oneway_sp$`All categories: Method of travel to work` /
+       mean(flow_cam_oneway_sp$`All categories: Method of travel to work`))
+
+df = flow_cam_oneway_sp@data
+write_csv(df, "/tmp/cam-oneway-updated-rl.csv")
