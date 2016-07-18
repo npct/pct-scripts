447001825f75d8b9c367f6e9ffba44670c25da63
robinlovelace <rob00x@gmail.com>
2016-07-18 23:29:22 +0100

    Use population weighted centroids, not zones, for lines

--- R/oneway-line-gen.R
+++ R/oneway-line-gen.R
@@ -27,11 +27,13 @@ flow_cam <- flow_cens[o & d, ] # subset OD pairs with o and d in study area
 # Remove flows with all < 10
 # flow_cam = flow_cam[flow_cam$`All categories: Method of travel to work` > 10,]
 # Convert to SpatialLines
-flow_cam_sp = od2line(flow = flow_cam, zones = zones)
+flow_cam_sp = od2line(flow = flow_cam, zones = cents)
+flow_cam_sp$dist = gprojected(flow_cam_sp, byid = T) / 1000
 plot(flow_cam_sp)
 flow_cam_sp_osgb = spTransform(flow_cam_sp, CRSobj = CRS("+init=epsg:27700"))
-flow_cam_sp$dist = gLength(flow_cam_sp_osgb, byid = T) / 1000
+flow_cam_sp$dist3 = gLength(flow_cam_sp_osgb, byid = T) / 1000
 summary(flow_cam_sp$dist)
+# cor(flow_cam_sp$dist, flow_cam_sp$dist3) # testing the method works
 flow_cam_sp = flow_cam_sp[flow_cam_sp$dist < 20 & flow_cam_sp$dist > 0.5,]
 plot(flow_cam_sp, lwd = flow_cam_sp$`All categories: Method of travel to work` /
        mean(flow_cam_sp$`All categories: Method of travel to work`))# Finding: 2304 lines
@@ -66,3 +68,19 @@ plot(flow_cam_oneway_sp, lwd = flow_cam_oneway_sp$`All categories: Method of tra
 
 df = flow_cam_oneway_sp@data
 write_csv(df, "/tmp/cam-oneway-updated-rl.csv")
+
+
+# side note: testing od2line
+df_test = flow_cam_sp@data
+summary(flow_cam_sp$dist) # only up to 20 km
+sp_test = od2line(df_test, cents)
+sp_test2 = od2line2(df_test, cents)
+proj4string(sp_test2) = proj4string(sp_test)
+bbox(sp_test)
+flow_cam_sp$dist4 = gprojected(sp_test, byid = T) / 1000
+flow_cam_sp$dist5 = gprojected(sp_test2, byid = T) / 1000
+plot(flow_cam_sp$dist4, flow_cam_sp$dist)
+plot(flow_cam_sp$dist5, flow_cam_sp$dist)
+plot(flow_cam_sp)
+plot(sp_test, col = "red", add = T)
+plot(sp_test2, col = "green", add = T)
