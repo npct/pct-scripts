799ffc8785ffb3a22e1ce7479a1905f6a6c27014
Robin <rob00x@gmail.com>
2016-07-27 22:56:18 +0100

    Update script to rebuild national lines

--- R/lines_oneway_shapes.R
+++ R/lines_oneway_shapes.R
@@ -4,6 +4,7 @@ source("../stplanr/R/overline.R")
 cents = geojsonio::geojson_read("../pct-bigdata/cents-scenarios.geojson", what = "sp")
 # load OD data - source http://wicid.ukdataservice.ac.uk/
 flow_cens = readr::read_csv("../pct-bigdata/wu03ew_v2.csv")
+flow_cens$id = paste(flow_cens$`Area of residence`, flow_cens$`Area of workplace`)
 nrow(flow_cens) # 2.4 m
 
 omatch = match(flow_cens$`Area of residence`, cents$geo_code)
@@ -90,11 +91,9 @@ summary(lines$all) # much higher average
 head(l$id)
 head(l_new$id)
 
-lines$geo_label_o	<- lines$geo_label_d	<- lines$dist_fast	<-
-lines$dist_quiet <- lines$time_fast	<- lines$time_quiet <-
-lines$cirquity <- lines$distq_f <- lines$avslope <-
-lines$co2_saving <- lines$busyness <- lines$avslope_q <-
-  NA
+# creat placeholder variable names
+lines$geo_label_o	<- lines$geo_label_d	<- lines$cirquity <- NA
+# lines$co2_saving <- lines$busyness <- # commented - additional variables will be added during merge
 
 newdat <- left_join(lines@data, l_new[!duplicated_idx], by="id")
 
@@ -129,8 +128,80 @@ rf = readRDS("../pct-bigdata/rf.Rds")
 rq = readRDS("../pct-bigdata/rf.Rds")
 
 head(rf)
-sel_rf_match = rf$id %in% lines$id
-summary(sel_rf_match)
-head(rf$id[sel_rf_match]) # ids in rf but not lines
 
+# rename vars to merge in - first remove duplicate names from lines
+lines$dist_fast	<- lines$dist_quiet <- lines$time_fast	<- lines$time_quiet <-
+  lines$distq_f <- lines$avslope_q <- lines$avslope <- NULL
+
+rfdata = dplyr::select(rf@data, id, dist_fast = length, avslope = av_incline, time_fast = time, busyness_fast = busyness)
+rqdata = dplyr::select(rq@data, id, dist_quiet = length, avslope_q = av_incline, time_quiet = time, busyness_quiet = busyness)
+
+# prepare for merge - analysis commented out
+# sel_rf_match = rf$id %in% lines$id
+# summary(sel_rf_match) # 8k lines don't match - distance?
+# ids_nomatch = rf$id[!sel_rf_match] # ids in rf but not lines
+# summary(ids_nomatch %in% flow_cens$id)
+# head(flow_cens[flow_cens$id %in% ids_nomatch, 1:5])
+# 
+# rf$msoa1 = stringr::str_sub(string = rf$id, start = 1, end = 9)
+# rf$msoa2 = stringr::str_sub(string = rf$id, start = 11, end = 19)
+# new_rf_ids = paste(rf$msoa2, rf$msoa1)
+# summary(new_rf_ids[!sel_rf_match] %in% lines$id)
+# new_ids_nomatch = new_rf_ids[!sel_rf_match] # ids in rf but not lines
+# summary(new_ids_nomatch %in% lines$id)
+# 
+# summary(sel_flow_nomatch)
+# summary(flow_cens[sel_flow_nomatch,]) 
+
+# merge the distances that *do* match
+newdat_rf = left_join(lines@data, rfdata, by = "id")
+sum(newdat_rf$all[!is.na(newdat_rf$dist_fast)], na.rm = T) # 98% flows captured ++
+cor(newdat_rf$dist_fast, newdat_rf$dist, use = "complete.obs") # very good correlation (0.98)
+newdat_rf_rq = left_join(newdat_rf, rq@data, by = "id")
+lines@data = newdat_rf_rq
+
+# add the missing routes - for subset of the data (cambridge) to test first
+cents = readRDS("../pct-data/cambridgeshire/c.Rds")
+o <- lines$msoa1 %in% cents$geo_code
+d <- lines$msoa2 %in% cents$geo_code
+lines_orig = lines
+lines <- lines[o & d, ] # subset OD pairs with o and d in study area
+rf <- rf[rf$id %in% lines$id,]
+rq <- rq[rq$id %in% lines$id,]
+sel_no_rf = is.na(lines$dist_fast)
+summary(sel_no_rf)
+rf_new = line2route(l = lines[sel_no_rf,], plan = "fastest")
+rf_new$id = lines$id[sel_no_rf]
+rf_new$nv = stplanr::n_vertices(rf_new)
+summary(rf_new$nv == 2) # check for failed lines
+rf_new@data = rf_new@data[match(names(rf), names(rf_new))]
+
+rf_updated = sbind(rf, rf_new)
+nrow(rf_updated) == nrow(lines) # now: 1 to 1 match with lines
+plot(order(rf_updated$id)) # shows ids are out
+plot(order(lines$id)) # correct ids
+rf_updated = rf_updated[order(rf_updated$id),]
+
+plot(lines[99,]) # check they match
+plot(rf_updated[99,], add = T) # yes
+
+# the same for the quietest routes
+sel_no_rq = is.na(lines$dist_fast)
+summary(sel_no_rq)
+rq_new = line2route(l = lines[sel_no_rq,], plan = "quietest")
+rq_new$id = lines$id[sel_no_rq]
+rq_new$nv = stplanr::n_vertices(rq_new)
+summary(rq_new$nv == 2) # check for failed lines
+rq_new@data = rq_new@data[match(names(rq), names(rq_new))]
+
+rq_updated = sbind(rq, rq_new)
+nrow(rq_updated) == nrow(lines) # now: 1 to 1 match with lines
+plot(order(rq_updated$id)) # shows ids are out
+plot(order(lines$id)) # correct ids
+rq_updated = rq_updated[order(rq_updated$id),]
+
+plot(lines[99,]) # check they match
+plot(rf_updated[99,], add = T) # yes
+
+# save for the local area
 
