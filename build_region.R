524b193144805a296da55df80ac107aeec95a73c
robinlovelace <rob00x@gmail.com>
2016-08-09 10:28:56 +0100

    Port cumbersome rnet code into separate script.
    Easier to rebuild data without running overline()

--- build_region.R
+++ build_region.R
@@ -129,33 +129,8 @@ if (rft_too_large){
   file.create(file.path(pct_data, region, "rft_too_large"))
 }
 
-rnet <- overline(rft, "bicycle")
-
-if(require(foreach) & require(doParallel)){
-  n_cores <- 4 # set max number of cores to 4
-  # reduce n_cores for 2 core machines
-  if(parallel:::detectCores() < 4)
-    n_cores <- parallel:::detectCores()
-  cl <- makeCluster(n_cores)
-  registerDoParallel(cl)
-  # foreach::getDoParWorkers()
-    # create list in parallel
-    rft_data_list <- foreach(i = scens) %dopar% {
-      rnet_tmp <- stplanr::overline(rft, i)
-      rnet_tmp@data[i]
-    }
-    # save the results back into rnet with normal for loop
-    for(j in seq_along(scens)){
-      rnet@data <- cbind(rnet@data, rft_data_list[[j]])
-    }
-    stopCluster(cl = cl)
-} else {
-  for(i in scens){
-    rnet_tmp <- overline(rft, i)
-    rnet@data[i] <- rnet_tmp@data[i]
-    rft@data[i] <- NULL
-  }
-}
+# source("R/generate_rnet.R") # comment out to avoid slow rnet build
+rnet = readRDS(file.path(pct_data, region, "rnet.Rds")) # uncomment if built
 
 # debug rnet so it is smaller and contains only useful results
 # summary(rnet) # diagnostic check of what it contains
@@ -194,7 +169,6 @@ cents@data$avslope <- NULL
 cents@data <- left_join(cents@data, zones@data)
 
 # # Save objects
-
 l@data = round_df(l@data, 5)
 l@data <- as.data.frame(l@data) # convert from tibble to data.frame
 # the next line diagnoses missing variables or incorrectly names variables
