70c4da1b3b38806d8a9b3c8fa6bdaaa81ea14b87
robinlovelace <rob00x@gmail.com>
2016-08-10 10:18:12 +0100

    Update generate_routes script for 'chunking'

--- R/generate_routes.R
+++ R/generate_routes.R
@@ -2,10 +2,10 @@ source("set-up.R")
 l = readRDS("../pct-bigdata/lines_oneway_shapes_updated.Rds")
 l = l[l$dist > 0,]
 
-# subset lines for cambridge (temporary - comment out for generating all routes)
-z = readRDS("../pct-data/cambridgeshire/z.Rds")
-l = l[z,]
-plot(l)
+# # subset lines for cambridge (temporary - comment out for generating all routes)
+# z = readRDS("../pct-data/cambridgeshire/z.Rds")
+# l = l[z,]
+# plot(l)
 
 rf = line2route(l = l, route_fun = "route_cyclestreet", plan = "fastest")
 rq = line2route(l = l, route_fun = "route_cyclestreet", plan = "quietest")
@@ -14,5 +14,40 @@ rq = line2route(l = l, route_fun = "route_cyclestreet", plan = "quietest")
 rf$id = l$id
 rq$id = l$id
 
-saveRDS(rf, "../pct-bigdata/rf_cam.Rds")
-saveRDS(rq, "../pct-bigdata/rq_cam.Rds")
+# breaking lines into n pieces
+n = 50
+x = 1:nrow(l)
+sel = split(x, cut(x, n, labels = FALSE))
+rf_n = as.list(1:n)
+num_names = formatC(1:n, flag = "0", width = 2)
+sel_names = paste0("rf", num_names, ".Rds")
+for(i in 32:n){
+  rf_n[[i]] = line2route(l = l[sel[[i]],], route_fun = "route_cyclestreet", plan = "fastest")
+  saveRDS(rf_n[[i]], paste0("../pct-bigdata/", sel_names[i]))
+}
+
+# bind the routes together
+f = list.files(path = "../pct-bigdata/", pattern = "rf[0-9]", full.names = T)
+for(i in 1:length(f)){
+  if(i == 1){
+    rf = readRDS(f[i])
+  }else{
+    rn = readRDS(f[i])
+    rf = sbind(rf, rn)
+  }
+}
+rf$id = l$id[1:nrow(rf)]
+
+# testing
+rq = readRDS("../pct-bigdata/rq_nat.Rds")
+summary(l$id == rq$id) # all match
+summary(rf$id == l$id[1:nrow(rf)])
+
+n = 999
+plot(l[n,])
+plot(rq[n,], add = T, col = "green")
+plot(rf[n,], add = T, col = "red")
+
+
+
+
