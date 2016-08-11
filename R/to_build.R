d84c2697305ecefd532736dfedd1c6d6a3b857fd
robinlovelace <rob00x@gmail.com>
2016-08-11 11:41:44 +0100

    Update to_build script

--- R/to_build.R
+++ R/to_build.R
@@ -5,7 +5,7 @@ library(lubridate)
 regions = readOGR("../pct-bigdata/regions.geojson", layer = "OGRGeoJSON")
 regions = as.character(regions$Region)
 i = 1
-start_date = as_date("2016-08-05")
+start_date = as_date("2016-08-09")
 file_date = file.mtime(paste0("../pct-data/", regions[i], "/rnet.Rds"))
 start_date < as_date(file_date)
 region_dates = data_frame(name = regions, date = as.Date(NA), to_rebuild = NA)
@@ -14,3 +14,10 @@ for(i in 1:length(regions)){
   region_dates$to_rebuild[i] = start_date > as_date(region_dates$date[i])
 }
 write_csv(region_dates, "to_rebuild.csv")
+
+to_rebuild_updated = read_csv("to_rebuild_updated.csv")
+to_rebuild_updated$date = region_dates$date
+recently_built = start_date < as_date(region_dates$date)
+to_rebuild_updated$to_rebuild[recently_built] = 0
+
+write_csv(to_rebuild_updated, "to_rebuild_updated.csv")
