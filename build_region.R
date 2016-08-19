8d33c231d627af7fb2e65a853be6feb40ca66cb2
Robin Lovelace <rob00x@gmail.com>
2016-08-19 17:36:06 +0100

    Revert shifting params to shared build, back in build_region.R

--- build_region.R
+++ build_region.R
@@ -1,17 +1,54 @@
-#This script is called by buildmaster.R to build each region
+# source("set-up.R") # load packages needed - commented as run in buildmaster
+start_time <- Sys.time() # for timing the script
 
-#If you wish to run this script on it own then uncomment these 2 lines
-# source("shared_build.R")
-# source("set-up.R")
+if(!exists("region")) region <- "cambridgeshire" # create LA name if none exists,  then set-up data repo
+pct_data <- file.path("..", "pct-data")
+pct_bigdata <- file.path("..", "pct-bigdata")
+pct_privatedata <- file.path("..", "pct-privatedata")
+pct_shiny_regions <- file.path("..", "pct-shiny", "regions_www")
+if(!file.exists(pct_data)) stop(paste("The pct-data repository cannot be found.  Please clone https://github.com/npct/pct-data in", dirname(getwd())))
+if(!file.exists(pct_bigdata)) stop(paste("The pct-bigdata repository cannot be found.  Please clone https://github.com/npct/pct-bigdata in", dirname(getwd())))
+scens <- c("govtarget_slc", "gendereq_slc", "dutch_slc", "ebike_slc")
 
-#Start of script
+# Set local authority and ttwa zone names
+region_path <- file.path(pct_data, region)
+if(!dir.exists(region_path)) dir.create(region_path) # create data directory
 
-# region specific build script
+params <- NULL # build parameters (saved for future reference)
+params$mflow <- 10 # minimum flow between od pairs to show for longer lines, high means fewer lines
+params$mflow_short <- 10 # minimum flow between od pairs to show for short lines, high means fewer lines
+params$mdist <- 20 # maximum euclidean distance (km) for subsetting lines
+params$max_all_dist <- 7 # maximum distance (km) below which more lines are selected
+params$buff_dist <- 0 # buffer (km) used to select additional zones (often zero = ok)
+# parameters related to the route network
+params$buff_geo_dist <- 100 # buffer (m) for removing line start and end points for network
+# params$min_rnet_length <- 2 # minimum segment length for the Route Network to display (may create holes in rnet)
+params$rft_keep = 0.05 # how aggressively to simplify the route network (higher values - longer to run but rnet less likely to fail)
+if(!exists("ukmsoas")){ # MSOA zones
+  ukmsoas <- readRDS(file.path(pct_bigdata, "ukmsoas-scenarios.Rds"))
+  ukmsoas$avslope = ukmsoas$avslope * 100
+}
+if(!exists("centsa")) # Population-weighted centroids
+  centsa <- readOGR(file.path(pct_bigdata, "cents-scenarios.geojson"), "OGRGeoJSON")
+centsa$geo_code <- as.character(centsa$geo_code)
+
+source('shared_build.R')
+
+# load in codebook data
+codebook_l = readr::read_csv("../pct-shiny/static/codebook_lines.csv")
+codebook_z = readr::read_csv("../pct-shiny/static/codebook_zones.csv")
+
 # select msoas of interest
 if(proj4string(region_shape) != proj4string(centsa))
   region_shape <- spTransform(region_shape, proj4string(centsa))
 cents <- centsa[region_shape,]
 zones <- ukmsoas[ukmsoas@data$geo_code %in% cents$geo_code, ]
+
+# load flow dataset, depending on availability
+if(!exists("flow_nat"))
+  flow_nat <- readRDS(file.path(pct_bigdata, "lines_oneway_shapes_updated.Rds"))
+  flow_nat <- flow_nat[flow_nat$dist > 0,]
+summary(flow_nat$dutch_slc / flow_nat$all)
 
 # Subset by zones in the study area
 o <- flow_nat$msoa1 %in% cents$geo_code
