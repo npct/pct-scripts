568204d4fa8f74952b158f7aaced25ba423b6386
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2016-10-10 15:58:55 +0100

    Merge Lines and Routes
    SHort file that takes a set of lines (straight) and mergers them with
    routes (along the road) it assumes that there are no common ids and thus
    the routes and lines must be in the same order. This is true for using
    the line2route function in stplanr, also checks the distance between
    routes and lines for when MSOA centroids are not near roads

---
+++ R/merge_lines_routes.R
@@ -0,0 +1,55 @@
+#####
+#This file merges lines and route data so that the routes contain the relavant origin and desination codes
+###
+# NOTE this file assumes the data in each file comes in exactly the same order
+#####
+
+#libraires
+library(sp)
+library(raster)
+library(maptools)
+library(leaflet)
+
+#Set up these parameters
+path <- "../pct-bigdata/msoa_rerun/oldway/"  #path to the working directory where everything is saved
+route_file <- "rq_EW_20-30" #name of the route file without .Rds
+line_file <- "lines_EW_20-30"  #name of the line file without .Rds
+
+# Load in Files
+routes <- readRDS(paste0(path,route_file,".Rds"))
+lines <- readRDS(paste0(path,line_file,".Rds"))
+
+#do some checks
+tests <- matrix(nrow = nrow(lines), ncol = 2)
+for(numb in 1:nrow(lines)){
+rcord <- routes@lines[[numb]]@Lines[[1]]@coords[c(1,nrow(routes@lines[[numb]]@Lines[[1]]@coords)),]
+lcord <- lines@lines[[numb]]@Lines[[1]]@coords[,]
+sdist <- geosphere::distHaversine(rcord[1,], lcord[1,])
+fdist <- geosphere::distHaversine(rcord[2,], lcord[2,])
+tests[numb,1] <-sdist
+tests[numb,2] <-fdist
+}
+print(paste0("The max difference in starting locations is ", as.integer(max(tests[,1]))," m in row number ",which.max(tests[,1])))
+print(paste0("The max difference in finishing locations is ", as.integer(max(tests[,2]))," m in row number ",which.max(tests[,2])))
+
+#plot the results
+print("plotting the largest errors for you")
+leaflet() %>% 
+  addTiles() %>% 
+  addPolylines(data = routes[which.max(tests[,1]),]) %>% 
+  addPolylines(data = lines[which.max(tests[,1]),])  %>%
+  addPolylines(data = routes[which.max(tests[,2]),]) %>% 
+  addPolylines(data = lines[which.max(tests[,2]),])
+
+#cbind them togther
+both <- cbind(routes,lines)
+both$serror <- tests[,1]
+both$ferror <- tests[,2]
+
+# Save Results
+saveRDS(both, file= paste0(path,"merge_",route_file,".Rds"))
+write.csv(both, file = paste0(path,"merge_",route_file,".csv"))
+
+#######################
+#End of Code
+#######################
