3744d4b0c2109906506a89ba324af3d48fa843f0
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2016-11-04 16:59:51 +0000

    Robin's changes
    Robin change this code and claims it works

--- R/merge_annas_into_lines.R
+++ R/merge_annas_into_lines.R
@@ -5,7 +5,10 @@
 # is then save out the result overwriting the original l_nat file
 
 require(dplyr)
-l_nat <- readRDS(file.path("..", "pct-bigdata", "l_nat_noScens.Rds")) # without scinario data
+l_nat_orig <- readRDS("../pct-bigdata/lines_oneway_shapes_updated.Rds")
+names(l_nat_orig)
+data_to_keep = data.frame(l_nat_orig@data["id"])
+l_nat_orig@data <- data_to_keep 
 rf <- readRDS(file.path("..", "pct-bigdata", "rf_nat.Rds"))
 rq <- readRDS(file.path("..", "pct-bigdata", "rq_nat.Rds"))
 l_anna = read.csv(file = "D:/Users/earmmor/OneDrive - University of Leeds/Cycling Big Data/xx-PCTAreaLines_TEMP/161103_PCTarealines_csv/pct_lines.csv")
@@ -13,13 +16,16 @@ l_anna = read.csv(file = "D:/Users/earmmor/OneDrive - University of Leeds/Cyclin
 
 l_anna_eng <- l_anna[grepl("E\\d{8}", l_anna$msoa1) & grepl("E\\d{8}", l_anna$msoa2),]
 l_anna_eng$id <- paste(l_anna_eng$msoa1, l_anna_eng$msoa2)
-duplicated_idx = names(l_nat) %in% names(l_anna_eng)
+# duplicated_idx = names(l_nat) %in% names(l_anna_eng)
 
 id <- paste(l_nat$msoa1, l_nat$msoa2)
 l_nat@data <- l_nat@data[!duplicated_idx]
 l_nat$id <- id
 
-l_nat_merged <- left_join(l_nat@data, l_anna_eng, by = c("id" = "id"))
+l_nat_merged <- left_join(l_nat_orig@data, l_anna_eng)
+l_nat_orig@data = l_nat_merged
+
+
 
 not_in_annas_data <- l_nat_merged[is.na(l_nat_merged$ebike_siw), ] # need to ask Anna why...
 
@@ -32,5 +38,6 @@ l_nat@data <- l_nat_merged[to_drop, ]
 rf@data <- rf[to_drop, ]
 rq@data <- rq[to_drop, ]
 
-saveRDS(l_nat, file.path("..", "pct-bigdata", "l_nat.Rds"))
+saveRDS(l_nat_orig, file.path("..", "pct-bigdata", "l_nat.Rds"))
+
 write.csv(not_in_annas_data, file = "../pct-bigdata/not_in_annas_data.csv")
