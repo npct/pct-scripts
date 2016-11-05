7b24db325b4f250f39055ca2eceffbd18d7425a1
Robin <rob00x@gmail.com>
2016-11-05 17:13:06 +0000

    Refactor join script

--- R/merge_annas_into_lines.R
+++ R/merge_annas_into_lines.R
@@ -4,40 +4,31 @@
 # it joins them together asuming that anna has the right data
 # is then save out the result overwriting the original l_nat file
 
-require(dplyr)
+library(dplyr)
 l_nat_orig <- readRDS("../pct-bigdata/lines_oneway_shapes_updated.Rds")
-names(l_nat_orig)
 data_to_keep = data.frame(l_nat_orig@data["id"])
-l_nat_orig@data <- data_to_keep 
-rf <- readRDS(file.path("..", "pct-bigdata", "rf_nat.Rds"))
-rq <- readRDS(file.path("..", "pct-bigdata", "rq_nat.Rds"))
-l_anna = read.csv(file = "D:/Users/earmmor/OneDrive - University of Leeds/Cycling Big Data/xx-PCTAreaLines_TEMP/161103_PCTarealines_csv/pct_lines.csv")
+l_nat = l_nat_orig
+l_nat@data = l_nat_orig@data[c("id", "is_two_way", "dist")]
 
-
-l_anna_eng <- l_anna[grepl("E\\d{8}", l_anna$msoa1) & grepl("E\\d{8}", l_anna$msoa2),]
-l_anna_eng$id <- paste(l_anna_eng$msoa1, l_anna_eng$msoa2)
-# duplicated_idx = names(l_nat) %in% names(l_anna_eng)
-
-id <- paste(l_nat$msoa1, l_nat$msoa2)
-l_nat@data <- l_nat@data[!duplicated_idx]
-l_nat$id <- id
-
-l_nat_merged <- left_join(l_nat_orig@data, l_anna_eng)
+# Add new lines
+rf <- readRDS("../pct-bigdata/rf_nat.Rds")
+rq <- readRDS("../pct-bigdata/rq_nat.Rds")
+f = "C://Users/georl/Dropbox/PCT/xx-PCTAreaLines_TEMP/161103_PCTarealines_csv.zip"
+unzip(zipfile = f)
+l_anna = readr::read_csv("161103_PCTarealines_csv/pct_lines.csv")
+l_anna$id <- paste(l_anna$msoa1, l_anna$msoa2)
+l_nat_merged <- left_join(l_nat@data, l_anna)
 l_nat_orig@data = l_nat_merged
 
-
-
 not_in_annas_data <- l_nat_merged[is.na(l_nat_merged$ebike_siw), ] # need to ask Anna why...
-
 in_annas_not_in_nat <- left_join(l_anna_eng, l_nat@data, by = c("id" = "id"))
 in_annas_not_in_nat <- in_annas_not_in_nat[is.na(in_annas_not_in_nat$is_two_way), ] # empty, we have it all
-
 to_drop <- !is.na(l_nat_merged$ebike_siw) # rows not in Anna's
 l_nat <- l_nat[to_drop, ]
 l_nat@data <- l_nat_merged[to_drop, ]
 rf@data <- rf[to_drop, ]
 rq@data <- rq[to_drop, ]
 
+# save output
 saveRDS(l_nat_orig, file.path("..", "pct-bigdata", "l_nat.Rds"))
-
 write.csv(not_in_annas_data, file = "../pct-bigdata/not_in_annas_data.csv")
