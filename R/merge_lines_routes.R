9829a8d9c8f1d996da7c9958a3ca7df0d2451f61
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2016-11-10 17:17:20 +0000

    Making Good
    Tweaks to new data so that it matches old data

--- R/merge_lines_routes.R
+++ R/merge_lines_routes.R
@@ -11,15 +11,20 @@ library(maptools)
 library(leaflet)
 
 #Set up these parameters
-path <- "../pct-bigdata/msoa_rerun/oldway/"  #path to the working directory where everything is saved
-route_file <- "rq_500_test2" #name of the route file without .Rds
-line_file <- "lines_500_test2"  #name of the line file without .Rds
+path <- "../pct-bigdata/"  #path to the working directory where everything is saved
+route_file <- "rq_nat" #name of the route file without .Rds
+line_file <- "l_nat_noScens"  #name of the line file without .Rds
+Derror <- 500 #number of meters difference between lines and route we are worried about
 
 # Load in Files
 routes <- readRDS(paste0(path,route_file,".Rds"))
 lines <- readRDS(paste0(path,line_file,".Rds"))
 
 #do some checks
+nrow(routes)
+nrow(lines)
+
+
 tests <- matrix(nrow = nrow(lines), ncol = 2)
 for(numb in 1:nrow(lines)){
 rcord <- routes@lines[[numb]]@Lines[[1]]@coords[c(1,nrow(routes@lines[[numb]]@Lines[[1]]@coords)),]
@@ -32,14 +37,20 @@ tests[numb,2] <-fdist
 print(paste0("The max difference in starting locations is ", as.integer(max(tests[,1]))," m in row number ",which.max(tests[,1])))
 print(paste0("The max difference in finishing locations is ", as.integer(max(tests[,2]))," m in row number ",which.max(tests[,2])))
 
+#Histograms!
+hist(tests[,1])
+hist(tests[,2])
+
+lines_error = lines[which(tests[,1]>Derror),]
+routes_error = routes[which(tests[,1]>Derror),]
+
 #plot the results
 print("plotting the largest errors for you")
-#leaflet() %>% 
-#  addTiles() %>% 
-#  addPolylines(data = routes[which.max(tests[,1]),]) %>% 
-#  addPolylines(data = lines[which.max(tests[,1]),])  %>%
-#  addPolylines(data = routes[which.max(tests[,2]),]) %>% 
-#  addPolylines(data = lines[which.max(tests[,2]),])
+leaflet() %>% 
+  addTiles() %>% 
+  addPolylines(data = lines_error) %>% 
+  addPolylines(data = routes_error)
+
 
 #cbind them togther
 both <- cbind(routes,lines)
@@ -47,7 +58,7 @@ both$serror <- tests[,1]
 both$ferror <- tests[,2]
 
 # Save Results
-saveRDS(both, file= paste0(path,"merge_",route_file,".Rds"))
+saveRDS(both, file= paste0(path,route_file,"_id.Rds"))
 write.csv(both, file = paste0(path,"merge_",route_file,".csv"))
 
 #######################
