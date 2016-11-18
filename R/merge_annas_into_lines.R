f667c5ba924f49782fe0d0ab122c2707b0a8153a
Malcolm Morgan <malcolmmorgan02@hotmail.com>
2016-11-18 16:16:15 +0000

    Simplify Anna Scen Data
    Join lines and scen data

--- R/merge_annas_into_lines.R
+++ R/merge_annas_into_lines.R
@@ -5,49 +5,53 @@
 # is then save out the result overwriting the original l_nat file
 
 library(dplyr)
-l_nat_orig <- readRDS("../pct-bigdata/lines_oneway_shapes_updated.Rds")
-data_to_keep = data.frame(l_nat_orig@data["id"])
-l_nat = l_nat_orig
-l_nat@data = l_nat_orig@data[c("id", "is_two_way", "dist")]
+path_to_routes <- "D:/Users/earmmor/OneDrive - University of Leeds/Cycling Big Data/ToNatBuild/"
+l_nat <- readRDS(file.path(path_to_routes, "lines_allonefile.Rds"))
+l_nat@data = l_nat@data[c("id", "is_two_way", "dist")]
 
 # Add new lines
-rf <- readRDS("../pct-bigdata/rf_nat.Rds")
-rq <- readRDS("../pct-bigdata/rq_nat.Rds")
+
+rf <- readRDS(file.path(path_to_routes, "rf_allonefile_withIDandANNA.Rds"))
+rq <- readRDS(file.path(path_to_routes, "rq_allonefile_withIDandANNA_rf.Rds"))
 # check they're the same
-n = 100001 # try many values
-plot(rf[n,]); plot(rq[n,], add = TRUE) # yes, good
-rf_geo_id = stplanr::line2df(rf)
+n = 76576 # try many values
+plot(rf[n,]); plot(rq[n,], add = TRUE); plot(l_nat[n,], add = TRUE)# yes, good
 
-
-f = "C://Users/georl/Dropbox/PCT/xx-PCTAreaLines_TEMP/161103_PCTarealines_csv.zip"
+path_to_dropbox <- "C://Users/georl/Dropbox"
+f = file.path(path_to_dropbox, "PCT/xx-PCTAreaLines_TEMP/161103_PCTarealines_csv.zip")
 unzip(zipfile = f)
 l_anna = readr::read_csv("161103_PCTarealines_csv/pct_lines.csv")
-l_anna$id <- paste(l_anna$msoa1, l_anna$msoa2)
-l_nat_merged <- left_join(l_nat@data, l_anna)
+l_anna$id <- paste(pmin(l_anna$msoa1, l_anna$msoa2), pmax(l_anna$msoa1, l_anna$msoa2))
+l_nat$id <- paste(pmin(l_nat$msoa1, l_nat$msoa2), pmax(l_nat$msoa1, l_nat$msoa2))
+l_nat_merged <- left_join(l_nat@data, l_anna, by = "id")
 l_nat@data = l_nat_merged
 
-# test compatibility of l_nat new with old data
-cor(l_nat$all, l_nat_orig$all, use = "complete.obs") # 1 = perfect fit
-cor(l_nat$govtarget_slc, l_nat_orig$govtarget_slc, use = "complete.obs") # 0.9982
-cor(l_nat$dutch_slc, l_nat_orig$dutch_slc, use = "complete.obs") # 0.9944
-summary(l_nat@data) # 12 nas in all vars - remove them
-l_nat = l_nat[!is.na(l_nat$all),]
-saveRDS(l_nat, "../pct-bigdata/lines_oneway_shapes_updated.Rds")
+# Remove flows without scenario data
+l_nat <- l_nat[!is.na(l_nat@data$govtarget_slc),]
+rf <- rf[!is.na(l_nat@data$govtarget_slc),]
+rq <- rq[!is.na(l_nat@data$govtarget_slc),]
 
-# Merge regional data
-fz = "../pct-bigdata/ukmsoas-scenarios.Rds"
-ukmsoas = readRDS(fz)
-z_anna = readr::read_csv("161103_PCTarealines_csv/pct_area.csv")
-z_cols_keep = names(ukmsoas)[!names(ukmsoas) %in% names(z_anna)]
-ukmsoas@data = ukmsoas@data[z_cols_keep][1:2]
-ukmsoas@data = dplyr::rename(ukmsoas@data,
-                             home_msoa = geo_code
-                             )
-ukmsoas@data = dplyr::inner_join(ukmsoas@data, z_anna)
-ukmsoas@data = dplyr::rename(ukmsoas@data,
-                                            geo_code = home_msoa,
-                                            geo_label = home_msoa_name
+l_nat@data = dplyr::rename(l_nat@data,
+                            all = all.y,
+                            msoa1 = msoa1.y,
+                            msoa2 = msoa2.y,
+                            light_rail = light_rail.y,
+                            train = train.y,
+                            bus = bus.y,
+                            taxi = taxi.y,
+                            motorbike = motorbike.y,
+                            car_driver = car_driver.y,
+                            car_passenger = car_passenger.y,
+                            bicycle = bicycle.y,
+                            foot = foot.y,
+                            other = other.y
 )
 
-names(ukmsoas)
-saveRDS(ukmsoas, fz)
+remove_cols <- function(df, col_regex){
+  df[,!grepl(col_regex, names(df))]
+}
+l_nat@data <- remove_cols(l_nat@data, "\\.x") # remove the dupped columns
+
+saveRDS(l_nat,"../pct-bigdata/msoa/l_nat.Rds")
+saveRDS(rf,"../pct-bigdata/msoa/rf_nat.Rds")
+saveRDS(rq,"../pct-bigdata/msoa/rq_nat.Rds")
