d0d40cb4b25d91ab759efa32b6db7246988a97ff
robinlovelace <rob00x@gmail.com>
2016-12-06 16:52:30 +0000

    Update regionsgen script

--- regionsgen.R
+++ regionsgen.R
@@ -3,7 +3,7 @@
 
 lapply(c("geojsonio", "sp"), library, character.only = T)
 
-regions = geojson_read("../pct-data/regions.geojson", what = "sp", stringsAsFactors = F)
+regions = geojson_read("../pct-shiny/regions_www/regions.geojson", what = "sp", stringsAsFactors = F)
 
 proj4string(regions)=CRS("+init=epsg:4326 +proj=longlat")
 
@@ -11,12 +11,13 @@ for(i in 1:length(regions)){
   region_shape = regions[i,]
   region_name = region_shape$Region
 
-  zones = readRDS(paste0(region_name, "/z.Rds"))
+  zones = readRDS(paste0("../pct-data/", region_name, "/z.Rds"))
   regions$pcycle[i] <- round(100 * sum(zones$bicycle) / sum(zones$all), 1)
   regions$govtarget_slc[i] <- round(100 * sum(zones$govtarget_slc) / sum(zones$all), 1)
   regions$gendereq_slc[i] <- round(100 * sum(zones$gendereq_slc) / sum(zones$all), 1)
   regions$dutch_slc[i] <- round(100 * sum(zones$dutch_slc) / sum(zones$all), 1)
   regions$ebike_slc[i] <- round(100 * sum(zones$ebike_slc) / sum(zones$all), 1)
-  regions$url <- paste0("./", region_name)
+  regions$url[i] <- paste0("./", region_name)
+  
 }
 geojson_write(regions, file = "../pct-shiny/regions_www/regions.geojson")
