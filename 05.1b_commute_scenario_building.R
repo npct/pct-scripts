245871b834ff751e58e7ed2ff9c3143792f626fa
AnnaGoodman1 <anna.goodman@lshtm.ac.uk>
2017-09-12 10:04:29 +0100

    update comments

--- 05.1b_commute_scenario_building.R
+++ 05.1b_commute_scenario_building.R
@@ -14,11 +14,12 @@ z_all_attributes <- read_csv(file.path(path_temp_scenario, purpose, geography, "
 od_all_attributes <- read_csv(file.path(path_temp_scenario, purpose, geography, "od_all_attributes_unrounded.csv"))
 lad_attributes <- read_csv(file.path(path_temp_scenario, purpose, "lad_all_attributes_unrounded.csv"))
 
+## OPEN CODEBOOKS
 z_codebook <- read_csv(file.path(path_codebooks, purpose, "z_codebook.csv"))
 od_l_rf_codebook <- read_csv(file.path(path_codebooks, purpose, "od_l_rf_codebook.csv"))
 lad_codebook <- read_csv(file.path(path_codebooks, purpose, "lad_codebook.csv"))
 
-## APPLY MINIMUM SIZE, ROUND AND SAVE
+## APPLY MINIMUM SIZE + ROUND, SUBSET TO CODEBOOK VARIABLES, SAVE
 z_all_attributes <- as.data.frame(apply(z_all_attributes, c(2), round_df), stringsAsFactors = F)
 z_all_attributes <- z_all_attributes[z_codebook$`Variable name`]
 write_csv(z_all_attributes,  file.path(path_outputs_national, purpose, geography, "z_all_attributes.csv"))
